7ad185589de1445f0cc8011828c8918a3a234b09
IBalitskiy <IBalitskiy@inno.tech>
2022-06-01 13:48:16 +0300

    w

--- src/test/scala/ru/vood/example/play/InnerDataClass.scala
+++ src/test/scala/ru/vood/example/play/InnerDataClass.scala
@@ -1,18 +1,51 @@
 package ru.vood.example.play
 
-import play.api.libs.json.{Json, OWrites, Reads}
+import play.api.libs.json.{JsString, JsValue, Json, OWrites, Reads, Writes}
 
 import java.util.Date
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+//import play.api.data.format.Formats.dateFormat
+import java.text.SimpleDateFormat
 
 case class InnerDataClass(
-                           birthDate: Date
+                           birthDate: DateTime
                          )
 
 object InnerDataClass {
 
+//  val dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+  val dateFormat = "yyyy-MM-dd"
+
+
+  implicit val jodaDateReads = Reads[DateTime](js =>
+    js.validate[String].map[DateTime](dtString =>
+      DateTime.parse(dtString, DateTimeFormat.forPattern(dateFormat))
+    )
+  )
+
+  implicit val jodaDateWrites: Writes[DateTime] = new Writes[DateTime] {
+  def writes(d: DateTime): JsValue = {
+
+    JsString(d.toString(dateFormat))
+  }
+  }
 
 
   implicit val writes: OWrites[InnerDataClass] = Json.writes[InnerDataClass]
 
   implicit val reads: Reads[InnerDataClass] = Json.reads[InnerDataClass]
+//  implicit val CaseObjectFormat = Json.format[InnerDataClass]
+
+
+
+//  private[this] def fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+//  implicit val format = Formats.dateFormat("yyyy-mm-dd")
+//  implicit val config: Configuration = Configuration.default
+
+//  implicit val dateEncoder: Encoder[Date] =
+//    Encoder[String].contramap(fmt.format)
+//  implicit val dateDecoder: Decoder[Date] =
+//    Decoder[String].emapTry(str => Try(fmt.parse(str)))
+//  implicit val dateReads = Reads.jodaDateReads("yyyy-MM-dd")
 }
--- src/test/scala/ru/vood/example/play/Test.scala
+++ src/test/scala/ru/vood/example/play/Test.scala
@@ -1,8 +1,9 @@
 package ru.vood.example.play
 
+import org.joda.time.DateTime
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-import play.api.libs.json.{JsError, JsResult, JsSuccess, Json}
+import play.api.libs.json.{JsError, JsObject, JsResult, JsSuccess, Json}
 
 import java.util.Date
 
@@ -10,17 +11,19 @@ class Test extends AnyFlatSpec with should.Matchers {
 
 
   "json DslJsonRandomMeta determenistic test" should "be called DataClass" in {
-    val innerData = InnerDataClass(new Date())
+    val innerData = InnerDataClass(new DateTime())
 
     val data = DataClass(true, Some("strval"), 1, innerData, List(innerData), Map("1" -> innerData, "2" -> innerData))
 //    println(Json.toJson(innerData))
 
 //    println(Json.toJson(data))
 
-    Json.toJsObject(data)
+    val value2: JsObject = Json.toJsObject(data)
+    val str: String = Json.stringify(value2)
+    println(str)
 
     val jsRes = Json.parse(
-    """{"bool":true,"str":"strval","int":1,"innerObj":{"birthDate":1649424304158},"innerList":[{"birthDate":1649424304158}],"innerMap":{"1":{"birthDate":1649424304158},"2":{"birthDate":1649424304158}}}""")
+    """{"bool":true,"str":"strval","int":1,"innerObj":{"birthDate":"2022-06-01"},"innerList":[{"birthDate":"2022-06-01"}],"innerMap":{"1":{"birthDate":"2022-06-01"},"2":{"birthDate":"2022-06-01"}}}""")
 
     val value1 = Json.fromJson[DataClass](jsRes)
 
@@ -29,7 +32,7 @@ class Test extends AnyFlatSpec with should.Matchers {
         println(path)
     }
 
-//    println(value1)
+//      println(value1)
 
   }
 
@@ -41,6 +44,8 @@ class Test extends AnyFlatSpec with should.Matchers {
 
     val value1 = Json.fromJson[DataClass](jsRes)
 
+    value1.get
+
 //    println(value1)
 
     value1 match {
@@ -50,7 +55,7 @@ class Test extends AnyFlatSpec with should.Matchers {
   }
 
   "json DslJsonRandomMeta determenistic test" should "be called InnerDataClass" in {
-    val innerData = InnerDataClass(new Date())
+    val innerData = InnerDataClass(new DateTime())
     println(Json.toJson(innerData))
     val value1 = Json.parse("""{"birthDate":1649425458139}""")
     println(Json.fromJson[InnerDataClass](value1))
